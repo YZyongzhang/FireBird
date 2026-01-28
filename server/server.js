7bcaf14eedb35d243a210b46352e197399394feb
getuanhui <getuanhui@bigai.ai>
2026-01-28 17:38:10 +0800

    增加了mk预览和文件切换

--- server/server.js
+++ server/server.js
@@ -1,33 +1,51 @@
-// server.js
-const express = require('express');
-const fs = require('fs');
+//express_demo.js 文件
+const express = require('express')
 const path = require('path');
-
-const app = express();
+const fs = require('fs');
+const app = express()
+app.use(express.static(path.join(__dirname, '../public')));
 const PORT = 3000;
+var files_dict = {}
+const data = fs.readFileSync('localstore/files_dict.json', 'utf8')
+const BASE_DIR = path.join(__dirname, '../localstore', 'markdown');
 
-// 解析 JSON 请求体
-app.use(express.json());
+files_dict = JSON.parse(data)
+console.log(files_dict)
 
-// 前端文件夹
-app.use(express.static('public'));
+function Add_File_Path_To_FilesDict(path){
+    var id = files_dict['Id'] + 1
+    files_dict['Path'][id] = path
+    files_dict["Id"] = id
+    fs.writeFileSync('localstore/files_dict.json', JSON.stringify(files_dict, null, 2), 'utf8');
+}
 
-// 保存 Markdown
-app.post('/save-markdown', (req, res) => {
-    const content = req.body.content;
+// Add_File_Path_To_FilesDict("localstore/markdown/README.md")
+console.log(files_dict)
 
-    if (!content) return res.status(400).json({ message: '内容为空' });
-
-    // 保存到当前目录的 markdown.md
-    fs.writeFile(path.join(__dirname, 'markdown.md'), content, 'utf8', (err) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ message: '保存失败' });
-        }
-        res.json({ message: '文件保存成功！' });
-    });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/html', 'main.html'))
 });
+// app.get('/login',(req , res ) => {
+//     res.sendFile(path.join(__dirname, '../public/html', 'main.html'))
+// })
+app.get('/api/get_file',(req , res ) => {
+    res.send(files_dict)
+})
+app.get('/getcontent', async (req, res) => {
+    const filePath = req.query.path;
+    if (!filePath) {
+        return res.status(400).json({ error: '缺少 path 参数' });
+    }
 
+    const safePath = path.join(BASE_DIR, path.basename(filePath));
+    try {
+        const content = await fs.promises.readFile(safePath, 'utf-8');
+        res.json({ content });
+    } catch (err) {
+        console.error(err);
+        res.status(404).json({ error: '文件不存在或读取失败' });
+    }
+});
 app.listen(PORT, () => {
-    console.log(`服务器启动：http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
